61719d7886a7d3265066ed62d71ed7c348d8d4a9
Ramon Boss <ramon.boss@outlook.com>
2016-11-17 18:08:55 +0100

    added empty functions  to implement and set them to global

---
+++ bigInteger.asm
@@ -0,0 +1,41 @@
+SECTION .text
+
+GLOBAL addition, subtraction, multiplication, readBigInteger, writeBigInteger, CopyBigInteger
+
+; RDI = address of first summand
+; RSI = address of second summand
+; return RDI
+addition:
+
+	ret
+
+; RDI = address of minuend
+; RSI = address of subtrahend
+; return RDI
+subtraction:
+
+	ret
+
+; RDI = address of first multiplier
+; RSI = address of second multiplier
+; return RDI 
+multiplication:
+
+	ret
+
+; RDI = address to read number
+readBitInteger:
+
+	ret
+
+; RDI = address of number to write
+writeBigInteger:
+
+	ret
+
+; RDI = address of original number
+; RSI = address to copy number
+copyBigInteger:
+
+	ret
+
