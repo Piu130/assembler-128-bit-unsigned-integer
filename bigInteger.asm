769a56c44489e47d0bfcbf2e2270d9f66d3bd3f8
Ramon Boss <ramon.boss@outlook.com>
2016-11-24 17:59:02 +0100

    implemented writeBigInteger

--- bigInteger.asm
+++ bigInteger.asm
@@ -1,10 +1,16 @@
 SECTION .data
 
 	BIGINTEGERLEN EQU 16
-	HEXDIGITS: db "FEDCBA9876543210"
+	HEXDIGITS: db "0123456789ABCDEF"
 	HEXDIGITSLEN equ $-HEXDIGITS
 
+	HEXSTR:	db "00000000000000000000000000000000",10
+	HEXLEN equ $-HEXSTR
+
 SECTION .bss
+
+	BUFFLEN equ 32		; we need 32 because 1 char needs 1 byte and 1 hex needs 4 bits
+	BUFF resb BUFFLEN	; temp buffer to read hex value
 
 SECTION .text
 
@@ -39,7 +45,6 @@ multiplication:
 
 	ret
 
-; RDI = address of string
 ; %1 = 0 for read, 1 for write
 %macro  _readWriteBigInteger 1
         push rsi
@@ -50,7 +55,7 @@ multiplication:
         mov rax, %1
         mov rsi, rdi
         mov rdi, 1
-        mov rdx, 2*BIGINTEGERLEN
+        mov rdx, BUFFLEN
         syscall
 
         pop rdx
@@ -66,9 +71,12 @@ readBigInteger:
 	push rcx
 	push rax
 
-	mov rcx, 2*BIGINTEGERLEN
+	mov r8, BUFF
+	mov rcx, BUFFLEN
 	.stringToHex
-		mov rax, [rdi+rcx]
+		mov rax, [BUFF+rcx]
+		cmp rax, 0
+		je .done		; check for 0 here (end of string)
 		sub rax, '0'
 		cmp rax, 9
 		jle .done
@@ -83,9 +91,34 @@ readBigInteger:
 
 ; RDI = address of number to write
 writeBigInteger:
+	push rcx
+	push rax
+	push rbx
+	push rdi
+
+	mov rcx, 16
+	.loopLetters
+		xor rax, rax			; clear rax
+		mov al, byte[rdi+rcx-1]		; mov current number to al
+		mov rbx, rax			; copy it for second nybble
+
+		and al, 0Fh			; mask out all but low nybble
+		mov al, byte[HEXDIGITS+rax]	; get character equivalent
+		mov byte[BUFF+rcx*2-1], al	; writes the number to its position
+
+		shr bl, 4			; shift high 4 bits of char into low 4 bits
+		mov bl, byte[HEXDIGITS+rbx]	; get character equivalent
+		mov byte[BUFF+rcx*2-2], bl	; writes the number to its position
+
+		loop .loopLetters
+
+	mov rdi, BUFF
 	_readWriteBigInteger 1
 
-
+	pop rdi
+	pop rbx
+	pop rax
+	pop rcx
 	ret
 
 ; RDI = address of original number
