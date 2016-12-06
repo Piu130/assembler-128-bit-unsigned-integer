f8bb99b48aa42c26b22930e33c3fb13f0c26c179
Ramon Boss <ramon.boss@outlook.com>
2016-12-06 20:02:55 +0100

    start of test file

--- main.asm
+++ main.asm
@@ -1,8 +1,12 @@
+
 SECTION .data
 
 	BIGINTEGERLEN EQU 16
 	TESTX TIMES 2 DQ 0xF894BFAC5677895F, 0x1123456789ABCDEF
 	TESTY TIMES 2 DQ 0x1000000000000001, 0xFEDCBA9876543210
+
+	ENTER: db "Enter %c",10,0
+	CALCOUTPUT: db "%c %c %c = ",10,0
 
 SECTION .bss
 
@@ -16,19 +20,63 @@ SECTION .bss
 SECTION .text
 
 EXTERN addition, subtraction, multiplication, readBigInteger, writeBigInteger, copyBigInteger
+EXTERN printf
 
-GLOBAL _start
+GLOBAL main
 
-_start:
+main:
 	nop
+
+	mov rdi, ENTER
+	mov rsi, 'X'
+	xor rax, rax
+	call printf
+
 	mov rdi, X
 	call readBigInteger
 
+	mov rdi, ENTER
+	mov rsi, 'Y'
+	xor rax, rax
+	call printf
+
+	mov rdi, Y
+	call readBigInteger
+
+	mov rdi, ENTER
+	mov rsi, 'Z'
+	xor rax, rax
+	call readBigInteger
+
+	mov rdi, X
+	mov rsi, R
+	call copyBigInteger
+
+	mov rdi, Y
+	mov rsi, S
+	call copyBigInteger
+
+	mov rdi, Z
+	mov rsi, T
+	call copyBigInteger
+
+	mov rdi, X
+	mov rsi, Y
+	call addition
+	mov rdi, CALCOUTPUT
+	mov rsi, 'X'
+	mov rdx, '+'
+	mov rcx, 'Y'
+	call printf
+	mov rdi, X
 	call writeBigInteger
 
-	mov rdi, TESTX
-	mov rsi, TESTY
-	call addition
+
+
+
+	;mov rdi, TESTX
+	;mov rsi, TESTY
+	;call addition
 
 
 	call exit
