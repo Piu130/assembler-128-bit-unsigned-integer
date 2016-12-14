d0d56144e0a8dbda1b090ebadb6869aa766e2ed5
Ramon Boss <ramon.boss@outlook.com>
2016-12-14 22:12:16 +0100

    implemented test class

--- main.asm
+++ main.asm
@@ -3,8 +3,9 @@ SECTION .data
 
 	BIGINTEGERLEN EQU 16
 
-	ENTER: db "Enter %c",10,0
-	CALCOUTPUT: db "%c %c %c = ",10,0
+	ENTER: db "Enter %c:",10,0
+	CALCOUTPUT: db "%c %c %c =",10,0
+	VARIS: db "%c is: ",10,0
 
 SECTION .bss
 
@@ -68,10 +69,74 @@ main:
 	mov rsi, 'X'
 	mov rdx, '+'
 	mov rcx, 'Y'
-	;call printf
+	xor rax, rax
+	call printf
 	mov rdi, X
 	call writeBigInteger
 
+	mov rdi, VARIS
+	mov rsi, 'Y'
+	xor rax, rax
+	call printf
+	mov rdi, Y
+	call writeBigInteger
+
+	mov rdi, X
+	mov rsi, Y
+	call subtraction
+	mov rdi, CALCOUTPUT
+	mov rsi, 'X'
+	mov rdx, '-'
+	mov rcx, 'Y'
+	xor rax, rax
+	call printf
+	mov rdi, X
+	call writeBigInteger
+
+        mov rdi, VARIS
+        mov rsi, 'Y'
+        xor rax, rax
+        call printf
+        mov rdi, Y
+        call writeBigInteger
+
+        mov rdi, R
+        mov rsi, S
+        call subtraction
+        mov rdi, CALCOUTPUT
+        mov rsi, 'R'
+        mov rdx, '-'
+        mov rcx, 'S'
+        xor rax, rax
+        call printf
+        mov rdi, R
+        call writeBigInteger
+
+        mov rdi, VARIS
+        mov rsi, 'S'
+        xor rax, rax
+        call printf
+        mov rdi, S
+        call writeBigInteger
+
+        mov rdi, T
+        mov rsi, Z
+        call multiplication
+        mov rdi, CALCOUTPUT
+        mov rsi, 'T'
+        mov rdx, '*'
+        mov rcx, 'Z'
+        xor rax, rax
+        call printf
+        mov rdi, T
+        call writeBigInteger
+
+        mov rdi, VARIS
+        mov rsi, 'Z'
+        xor rax, rax
+        call printf
+        mov rdi, Z
+        call writeBigInteger
 
 	call exit
 
